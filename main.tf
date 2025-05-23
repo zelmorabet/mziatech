0cacdcea887808bc71d80e7d53e98bfc15e03fd9
zelmorabet <61009201+zelmorabet@users.noreply.github.com>
2025-05-23 19:48:47 -0400

    Update main.tf
    change name resource

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ variable "access_key" {}
 variable "secret_key" {}
 variable "subnet_id" {}
 
-resource "aws_instance" "terraform-cloud-demo" {
+resource "aws_instance" "mziatech" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id   = var.subnet_id
